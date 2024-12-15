5cba940321e4c6472fab67c20eef32c39572faf1
Puffa05 <knipplasse@gmail.com>
2024-12-15 18:10:18 +0100

    Update snowflakes.js

--- snowflakes.js
+++ snowflakes.js
@@ -7,13 +7,13 @@ let snowflakes = (function() {
 	///////////////////////////// configuration ////////////////////////////
 
 	const defaultConfig = {
-		amount: 150, // amount of flakes
+		amount: 100, // amount of flakes
 		color: ['#AAAACC', '#DDDDFF', '#CCCCDD', '#F3F3F3', '#F0FFFF'], // random colors
 		fontType: ['Arial Black', 'Arial Narrow', 'Times', 'Comic Sans MS'], //	random fonts
 		flakeChar: '*', // char used for flake
-		speed: .05, // speed of flakes
-		size: [15, 30], // minimum/maximum flake font size
-		drift: 100, // horizontal drift
+		speed: .07, // speed of flakes
+		size: [8, 25], // minimum/maximum flake font size
+		drift: 50, // horizontal drift
 	};
 	
 	///////////////////////////// private vars /////////////////////////////
